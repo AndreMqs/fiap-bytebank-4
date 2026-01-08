5be8bb34be4262a5c096fd38b12223d335d34ee1
Andre Camara <andre.m.camara@hotmail.com>
2026-01-08 19:00:05 -0300

    Adding global styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../infra/react-query/queryClient'
 import RouterApp from './router'
+import './App.styles.scss'
 
 export default function App() {
   return (
